1b07d975a0d483b085501bb92e07b972d0884b91
Abigail Clements <Abby@UberActivist.com>
2018-07-23 19:12:53 -0500

    More additions

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ from time import sleep
 
 # Config
 nws_feed = "https://alerts.weather.gov/cap/us.php?x=1"
-alert_threshold = "Severe"
+alert_thresholds = ("Moderate","Severe")
 
 
 # Local Data
 recent_date = None
 
+def get_recent_date(feed):
+    global recent_date
+    recent_date = feed.entries[0].updated
+    for i in feed.entries:
+        if i.updated < recent_date:
+            recent_date = i.updated
+
 
 def main():
+    global recent_date
+
     current_feed = feedparser.parse(nws_feed)
+    if recent_date is None:
+        print("First run, getting the most recent date.")
+        get_recent_date(current_feed)
+
     for i in current_feed.entries:
-        global recent_date
-        if recent_date is None:
-            recent_date = i.updated
-            print("First run, picked a date and stuck with it.")
-            break
-        elif i.cap_severity == alert_threshold:
+        if i.cap_severity in alert_thresholds:
             if i.updated < recent_date:
                 print("{} in {}".format(i["title"], i["cap_areadesc"]))
                 recent_date = i.updated
                 print("Date condition met.")
-            else:
-                print("Date condition not met.")
+            """else:
+                print("Date condition not met.")"""
     sleep(10)
     main()
 
